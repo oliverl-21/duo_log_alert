475743c006549e5a0810a6700ac7f29c5bfc7428
oliverl-21 <77393143+oliverl-21@users.noreply.github.com>
2023-11-02 14:16:53 +0100

    update

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
-
 var nopt = require('nopt')
 const duo_api = require('@duosecurity/duo_api')
 const moment = require('moment')
+require('dotenv').config()
 var parsed = nopt({
   'ikey': [String],
   'skey': [String],
   'host': [String],
   'hook': [String]
-}, [], process.argv, 2)
+}, [], process.argv, 0)
 const { IncomingWebhook } = require('@slack/webhook')
 moment.locale('de')
-var requirements_met = (parsed.ikey && parsed.skey && parsed.host)
 var timenow = moment().valueOf()
 var timebefore = moment().subtract(3, 'days').valueOf()
 
+if ('duo_ikey' in process.env) {
+  var duo_ikey = process.env.duo_ikey
+} else if (parsed.ikey != '' || parsed.ikey !== 'undefined') {
+  var duo_ikey = parsed.ikey
+} else {
+  console.log('DUO ikey is missing')
+  proces.exit(1)
+}
+
+if ('duo_skey' in process.env) {
+  var duo_skey = process.env.duo_skey
+} else if (parsed.skey != '' || parsed.skey !== 'undefined') {
+  var duo_skey = parsed.skey
+} else {
+  console.log('DUO skey is missing')
+  process.exit(1)
+}
+
+if ('duo_host' in process.env) {
+  var duo_host = process.env.duo_host
+} else if (parsed.host != '' || parsed.host !== 'undefined') {
+  var duo_host = parsed.host
+} else {
+  console.log('DUO host is missing')
+  process.exit(1)
+}
+
+if ('hook' in process.env) {
+  var hook = process.env.hook
+} else if (parsed.hook != '' || parsed.hook !== 'undefined') {
+  var hook = parsed.hook
+} else {
+  console.log('Slack hook is missing')
+  process.exit(1)
+}
+
+// resoncode lookuptable
 const reasoncodes = {
   user_mistake: 'Fat Fingered',
   user_marked_fraud: 'Fraud ALARM!',
@@ -29,10 +65,12 @@ const reasoncodes = {
   locked_out: 'User locked',
   user_disabled: 'User disabled',
   user_cancelled: 'User cancelled Request'
-
 }
+
+// reasconcode conversion function
 const errorlookup = (reason) => reasoncodes[reason] || 'unknown Reason'
-//
+
+var requirements_met = (duo_ikey && duo_skey && duo_host)
 if (!requirements_met) {
   console.error('Missing required option.\n')
 }
@@ -59,31 +97,37 @@ Options:
     process.exit(1)
   }
 }
-const url = 'https://hooks.slack.com/services/' + parsed.hook
 
+// Slack Webhook function
+const url = 'https://hooks.slack.com/services/' + parsed.hook
 const webhook = new IncomingWebhook(url)
+function send_message(message) {
+  console.log(message)
+  if (hook != '' || hook !== 'undefined') {
+  async () => {await webhook.send({text: message})}
+  }
+}
 
 // Send the notification
-var client = new duo_api.Client(parsed.ikey, parsed.skey, parsed.host)
+var client = new duo_api.Client(duo_ikey, duo_skey, duo_host)
 client.jsonApiCall(
   'GET', '/admin/v2/logs/authentication', {'maxtime': timenow, 'mintime': timebefore, 'results': 'denied,fraud'},
   function (res) {
     if (res.stat !== 'OK') {
-      console.error('API call returned error: ' +
-          res.message)
+      console.error('API call returned error: ' + res.message)
       process.exit(1)
     }
     res = res.response
     for (var i in res) {
       for (n = 0; n < res[i].length; n++) {
-        console.log('User: ' + res[i][n].user.name + ' ' + errorlookup(res[i][n].reason)),
-        (async () => {
-          await webhook.send({text: 'User: ' + res[i][n].user.name + ', ' + errorlookup(res[i][n].reason) + ' at ' + moment(res[i][n].isotimestamp).format('LTS') + '\n from: ' + res[i][n].access_device.ip + '\n Location: ' + res[i][n].access_device.location.country})
-        }
-        )()
+         let message = send_message(
+          'User: ' + res[i][n].user.name + ', ' + 
+          '\nReason: ' + errorlookup(res[i][n].reason) + ' at ' + moment(res[i][n].isotimestamp).format('LTS') + 
+          '\nfrom IP: ' + res[i][n].access_device.ip + 
+          '\nAccess Location: ' + res[i][n].access_device.location.country +
+          '\n'
+          );
       }
-
-      // console.log(res.authlogs);
     }
   }
 )
